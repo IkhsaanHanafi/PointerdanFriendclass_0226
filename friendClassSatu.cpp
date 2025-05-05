8a48dfd4c3519b51d4704a882bb66393b085124d
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2025-05-05 21:11:37 +0700

    friendlanjutanpart2

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -68,3 +68,32 @@ int main()
     setNama(joko, "Kairi Kumar");
     return 0;
 }
+
+class pelajar;
+class manusia 
+{
+public:
+    void showNilaiPelajar(pelajar &x);
+};
+
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar &x);
+};
+
+void manusia::showNilaiPelajar(pelajar &x)
+{
+    cout << x.nilai;
+}
+int main()
+{
+    manusia budi;
+    pelajar pbudi;
+    budi.showNilaiPelajar(pbudi);
+    return 0;
+}
