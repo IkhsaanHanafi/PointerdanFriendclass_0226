50a44b15ab58ac62ea01098f74142c28a1711c3e
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2025-05-05 19:22:57 +0700

    friendClass

--- pointerdua.cpp
+++ pointerdua.cpp
@@ -10,3 +10,19 @@ public:
         cout << "No induk = " << nim << endl;
     }
 };
+
+int main()
+{
+    mahasiswa mhs; // object mhs
+    mhs.nim = 2024;
+    mhs.showNim(); // Member Access Operator
+
+    mahasiswa &refMhs = mhs; // Pointer Reference refMhs
+    refMhs.nim = 2;         // Member Access Operator 
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; // pointer Dereference pMhs
+    pMhs->nim =3;          // Arrow Operator
+    pMhs->showNim();
+    return 0;
+}
