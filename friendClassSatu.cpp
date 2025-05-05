36baee7fb4afaa444d8626be24fb22302dd28c6c
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2025-05-05 21:07:36 +0700

    friendlanjutanpart1

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -45,4 +45,26 @@ int main()
     joko_siswa.setId(1);
     joko_siswa.displayAll(joko);
     return 0;
+};
+
+class mahasiswa
+{
+private:
+    string nama;
+
+public:
+    friend void setNama(mahasiswa &a, string);
+};
+
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main()
+{
+    mahasiswa joko;
+    setNama(joko, "Kairi Kumar");
+    return 0;
 }
